49019ace9ff20b8770d271f7514906bbc6943417
mintyry <gitmintyry@gmail.com>
2023-10-17 16:04:50 -0700

    added questions for prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,60 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const markdown = require()
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'appName',
+        message: 'What is the name of your app?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a short description of this project (what, why, and how).'
+    },
+    {
+        type: 'list',
+        name: 'licenses',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What command should be run to conduct tests?',
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage instructions.',
+        default: 'npm start' //is this correct?
+    },
+    {
+        type: 'input',
+        name: 'collab',
+        message: 'If a collab, how does one contribute to repo?',
+        default: 'Fork repo and create pull request'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

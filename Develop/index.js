03de6fc07071a040535be4d15d7567c89f2e4ee9
mintyry <gitmintyry@gmail.com>
2023-10-17 18:32:23 -0700

    added prompt and writeFile

--- Develop/index.js
+++ Develop/index.js
@@ -1,66 +1,78 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const markdown = require()
+const generateMarkdown = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'appName',
-        message: 'What is the name of your app?'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please provide a short description of this project (what, why, and how).'
-    },
-    {
-        type: 'list',
-        name: 'licenses',
-        message: 'What kind of license should your project have?',
-        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'What command should be run to install dependencies?',
-        default: 'npm i'
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'What command should be run to conduct tests?',
-        default: 'npm test'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Please provide usage instructions.',
-        default: 'npm start' //is this correct?
-    },
-    {
-        type: 'input',
-        name: 'collab',
-        message: 'If a collab, how does one contribute to repo?',
-        default: 'Fork repo and create pull request'
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is your GitHub username?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address?'
-    }
-];
+const questions =[
+        {
+            type: 'input',
+            name: 'appName',
+            message: 'What is the name of your app?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a short description of this project (what, why, and how).'
+        },
+        {
+            type: 'list',
+            name: 'licenses',
+            message: 'What kind of license should your project have?',
+            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What command should be run to install dependencies?',
+            default: 'npm i'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What command should be run to conduct tests?',
+            default: 'npm test'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide usage instructions.',
+            default: 'npm start' //is this correct?
+        },
+        {
+            type: 'input',
+            name: 'collab',
+            message: 'If a collab, how does one contribute to repo?',
+            default: 'Fork repo and create pull request'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?'
+        }
+    ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(function (data){
+            fs.writeFile('readMe.md', generateMarkdown(data), function(err){
+                if (err) {
+                    console.error(err);
+                }
+                console.log('Success!')
+            })
+        })
+
+}
 
 // Function call to initialize app
 init();
